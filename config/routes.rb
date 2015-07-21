51bd03ea25238c2ee2519e818a7d4822d5b9ccc2
chan_ume <dottobasic673@gmail.com>
2015-07-21 10:03:22 +0000

    heroku deproy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   #get 'messages/index'
   root "messages#index"
-  resources :messages , except: [:create, :new]
+  resources :messages , except: [:index, :new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
