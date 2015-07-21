83836da0f247b6229bcb4178eb8c29625a656fd5
chan_ume <dottobasic673@gmail.com>
2015-07-21 10:49:16 +0000

    age complete

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ActiveRecord::Base
     validates :name, length: { maximum: 20}, presence: true
     validates :body, length: { minimum: 2, maximum: 30 }, presence: true
+    validates :age, numericality: true, presence: true
 end
 
